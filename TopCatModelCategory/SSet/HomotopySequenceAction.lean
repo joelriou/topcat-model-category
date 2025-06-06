1f3b0e84cc0d0178e4d5eff6a4f53758e65d82b8
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-06-06 22:52:24 +0200

    wip

--- TopCatModelCategory/SSet/HomotopySequenceAction.lean
+++ TopCatModelCategory/SSet/HomotopySequenceAction.lean
@@ -65,17 +65,57 @@ lemma nonempty' (e₁ : E _⦋0⦌) (h₁ : p.app _ e₁ = b₁) :
 
 variable {p f}
 
-/-def comp [IsFibrant B] {b₂ : B _⦋0⦌} {f₀₁ : Edge (.mk b₀) (.mk b₁)}
+noncomputable def comp {b₂ : B _⦋0⦌} {f₀₁ : Edge (.mk b₀) (.mk b₁)}
     {e₀ e₁ e₂ : E _⦋0⦌} (h₀₁ : FiberActionStruct p f₀₁ e₀ e₁)
     {f₁₂ : Edge (.mk b₁) (.mk b₂)}
     (h₁₂ : FiberActionStruct p f₁₂ e₁ e₂)
     {f₀₂ : Edge (.mk b₀) (.mk b₂)}
     (h : Edge.CompStruct f₀₁ f₁₂ f₀₂) :
-    FiberActionStruct p f₀₂ e₀ e₂ := sorry
+    FiberActionStruct p f₀₂ e₀ e₂ := Nonempty.some (by
+  obtain ⟨φ, hφ₁, hφ₂⟩ := horn₂₁.isPushout.exists_desc h₀₁.map h₁₂.map (by simp)
+  have sq : CommSq φ Λ[2,1].ι p h.map := ⟨by
+    apply horn₂₁.isPushout.hom_ext
+    · simp [reassoc_of% hφ₁]
+    · simp [reassoc_of% hφ₂]⟩
+  exact ⟨{
+    map := stdSimplex.δ 1 ≫ sq.lift
+    δ₀_map := by
+      have h₁ := stdSimplex.{u}.δ_comp_δ_self (n := 0) (i := 0)
+      dsimp at h₁
+      have h₂ := horn₂₁.ι₁₂ ≫= sq.fac_left
+      rw [horn.ι_ι_assoc, hφ₂] at h₂
+      rw [← reassoc_of% h₁, h₂, δ₀_map]
+    δ₁_map := by
+      have h₁ := stdSimplex.{u}.δ_comp_δ_self (n := 0) (i := 1)
+      dsimp at h₁
+      have h₂ := horn₂₁.ι₀₁ ≫= sq.fac_left
+      rw [horn.ι_ι_assoc, hφ₁] at h₂
+      rw [reassoc_of% h₁, h₂, δ₁_map]
+  }⟩)
 
-def symm [IsFibrant B] {e₀ e₁ : E _⦋0⦌} (h : FiberActionStruct p f e₀ e₁)
+noncomputable def symm {e₀ e₁ : E _⦋0⦌} (h : FiberActionStruct p f e₀ e₁)
     {f' : Edge (.mk b₁) (.mk b₀)} (hff' : Edge.CompStruct f' f (.id (.mk b₁))) :
-    FiberActionStruct p f' e₁ e₀ := sorry
+    FiberActionStruct p f' e₁ e₀ := Nonempty.some (by
+  obtain ⟨φ, hφ₁, hφ₂⟩ := horn₂₂.isPushout.exists_desc (const e₁) h.map (by simp)
+  have sq : CommSq φ Λ[2,2].ι p hff'.map := ⟨by
+    apply horn₂₂.isPushout.hom_ext
+    · simp [reassoc_of% hφ₁, h.app_one]
+    · simp [reassoc_of% hφ₂]⟩
+  exact ⟨{
+    map := stdSimplex.δ 2 ≫ sq.lift
+    δ₀_map := by
+      have h₁ := stdSimplex.{u}.δ_comp_δ (n := 0) (i := 0) (j := 1) (by simp)
+      dsimp at h₁
+      have h₂ := horn₂₂.ι₁₂ ≫= sq.fac_left
+      rw [horn.ι_ι_assoc] at h₂
+      rw [reassoc_of% h₁, h₂, hφ₂, δ₁_map]
+    δ₁_map := by
+      have h₁ := stdSimplex.{u}.δ_comp_δ (n := 0) (i := 1) (j := 1) (by simp)
+      dsimp at h₁
+      have h₂ := horn₂₂.ι₀₂ ≫= sq.fac_left
+      rw [horn.ι_ι_assoc] at h₂
+      rw [reassoc_of% h₁, h₂, hφ₁, comp_const]
+  }⟩)
 
 noncomputable def unique
     [IsFibrant B] {e₀ e₀' e₁ e₁' : E _⦋0⦌} (h : FiberActionStruct p f e₀ e₁)
@@ -86,9 +126,22 @@ noncomputable def unique
   Nonempty.some (by
     obtain ⟨g, ⟨hg⟩⟩ := Edge.CompStruct.left_inverse f
     exact ⟨comp (comp (h.symm hg) he (Edge.CompStruct.compId g))
-      h' (hg.assoc' hff'.homotopyL (.idComp _))⟩)-/
+      h' (hg.assoc' hff'.homotopyL (.idComp _))⟩)
 
 end FiberActionStruct
+
+section
+
+variable [Fibration p]
+
+noncomputable def fiberAction (e₀ : E _⦋0⦌) (h₀ : p.app _ e₀ = b₀) : E _⦋0⦌ :=
+  (FiberActionStruct.nonempty p f e₀ h₀).choose
+
+noncomputable def fiberActionStruct (e₀ : E _⦋0⦌) (h₀ : p.app _ e₀ = b₀) :
+    FiberActionStruct p f e₀ (fiberAction p f e₀ h₀) :=
+  (FiberActionStruct.nonempty p f e₀ h₀).choose_spec.some
+
+end
 
 end KanComplex.FundamentalGroupoid
 
