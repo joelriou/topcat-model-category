b055780e5e13cb86d83f42e2c60a368ef75d49b6
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-07-16 23:16:19 +0200

    note about a wrong statement

--- TopCatModelCategory/TopCat/ToTopExact.lean
+++ TopCatModelCategory/TopCat/ToTopExact.lean
@@ -4,6 +4,8 @@ open CategoryTheory Limits
 
 namespace SSet
 
+-- this is wrong: this is true only when we consider
+-- the function from simplicial sets to Kelley spaces
 instance : PreservesFiniteLimits toTop := sorry
 
 end SSet
