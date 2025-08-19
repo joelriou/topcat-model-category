2261dd20549f9121da470d898604e200bb37b07d
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-08-20 02:39:17 +0400

    wip

--- TopCatModelCategory/SSet/AffineMap.lean
+++ TopCatModelCategory/SSet/AffineMap.lean
@@ -1,20 +1,27 @@
 import TopCatModelCategory.TopCat.Adj
 import Mathlib.Analysis.Normed.Module.Basic
 
-open CategoryTheory Simplicial
+open CategoryTheory Simplicial Opposite
 
 universe v u
 
 namespace SimplexCategory.toTopObj
 
-variable {n : ℕ} {E : Type v} [AddCommGroup E] [Module ℝ E]
-  (f : ⦋n⦌.toTopObj → E)
+variable {n m : SimplexCategory}
 
-def vertex (i : Fin (n + 1)) : ⦋n⦌.toTopObj :=
+def vertex (i : Fin (n.len + 1)) : n.toTopObj :=
   ⟨fun j ↦ if j = i then 1 else 0, by simp [toTopObj]⟩
 
+@[simp]
+lemma toTopMap_vertex (f : n ⟶ m) (i : Fin (n.len + 1)) :
+    toTopMap f (vertex i) = vertex (f i) := by
+  dsimp [toTopMap, vertex]
+  aesop
+
+variable {E : Type v} [AddCommGroup E] [Module ℝ E] (f : n.toTopObj → E)
+
 def IsAffine : Prop :=
-  ∀ (x : ⦋n⦌.toTopObj), f x = ∑ (i : Fin (n + 1)), (x.1 i : ℝ) • f (vertex i)
+  ∀ (x : n.toTopObj), f x = ∑ (i : Fin (n.len + 1)), (x.1 i : ℝ) • f (vertex i)
 
 end SimplexCategory.toTopObj
 
@@ -25,15 +32,30 @@ variable {X : SSet.{u}} {E : Type v} [AddCommGroup E] [Module ℝ E]
 
 namespace IsAffineAt
 
-noncomputable def map {n : ℕ} (x : X _⦋n⦌) : ⦋n⦌.toTopObj → E :=
-  f.comp
-    (Function.comp (toTopSimplex.inv.app _ ≫ toTop.map (yonedaEquiv.symm x)) ULift.up)
+noncomputable def map {n : SimplexCategory} (x : X.obj (op n)) : n.toTopObj → E :=
+  f.comp (Function.comp
+    (toTopSimplex.inv.app _ ≫ toTop.map (yonedaEquiv.symm x)) ULift.up)
 
 end IsAffineAt
 
-def IsAffineAt {n : ℕ} (x : X _⦋n⦌) : Prop :=
+def IsAffineAt {n : SimplexCategory} (x : X.obj (op n)) : Prop :=
   SimplexCategory.toTopObj.IsAffine (IsAffineAt.map f x)
 
-def IsAffine : Prop := ∀ ⦃n : ℕ⦄ (x : X _⦋n⦌), IsAffineAt f x
+def IsAffine : Prop := ∀ ⦃n : SimplexCategory⦄ (x : X.obj (op n)), IsAffineAt f x
+
+def isAffine : X.Subcomplex where
+  obj := fun ⟨n⟩ ↦ setOf (fun x ↦ IsAffineAt f x)
+  map := sorry
+
+@[simp]
+lemma mem_isAffine_iff {n : SimplexCategory} (x : X.obj (op n)) :
+    x ∈ (isAffine f).obj _ ↔ IsAffineAt f x := Iff.rfl
+
+lemma isAffine_iff_eq_top : IsAffine f ↔ isAffine f = ⊤ := by
+  refine ⟨fun h ↦ ?_, fun h ↦ ?_⟩
+  · ext ⟨n⟩ x
+    simpa using h x
+  · intro n x
+    simp [← mem_isAffine_iff, h]
 
 end SSet
