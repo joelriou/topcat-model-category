17886fb61a87e6a731d5d5ff4d1b0e45c5000da5
Joël Riou <joel.riou@universite-paris-saclay.fr>
2025-09-16 10:29:13 +0200

    wip

--- TopCatModelCategory/TopCat/SdCompatibility.lean
+++ TopCatModelCategory/TopCat/SdCompatibility.lean
@@ -3,7 +3,7 @@ import TopCatModelCategory.SSet.Mesh
 
 universe u
 
-open Simplicial
+open CategoryTheory Simplicial
 
 namespace SSet
 
@@ -14,6 +14,15 @@ variable {X : SSet.{u}} [X.IsWeaklyPolyhedralLike]
 
 @[simp]
 lemma f_comp_toTopSdIso_hom : f.f ∘ (toTopSdIso X).hom = f.sd.f := by
+  have (x : X.N) :
+      f.f ∘ (toTopSdIso X).hom ∘ toTop.map (sd.map (yonedaEquiv.symm x.simplex)) =
+        f.sd.f ∘ toTop.map (sd.map (yonedaEquiv.symm x.simplex)) := by
+    ext y
+    refine (congr_arg f.f (ConcreteCategory.congr_hom
+      (N.toTop_map_sd_map_yonedaEquiv_symm_simplex_comp_toTopSdIso_hom.{u} x) y)).trans ?_
+    dsimp
+    sorry
+  have := @N.toTop_map_sd_map_yonedaEquiv_symm_simplex_comp_toTopSdIso_hom.{u}
   sorry
 
 @[simp]
