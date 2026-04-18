813338a8c88cfe0096deed7e3ba7daf92d4a1c71
Joël Riou <joel.riou@universite-paris-saclay.fr>
2026-04-18 14:55:30 +0200

    wip

--- TopCatModelCategory/Test.lean
+++ TopCatModelCategory/Test.lean
@@ -11,11 +11,18 @@ example : ModelCategory SSet.{u} := SSet.modelCategoryQuillen.inst
 #print axioms TopCat.modelCategory.inst
 #print axioms SSet.modelCategoryQuillen.inst
 
-#check CategoryTheory.MorphismProperty.IsCardinalForSmallObjectArgument
+#check TopCat.fibration_of_isOpenCover
+#check SSet.AffineMap
+#check SSet.AffineMap.sd
+#check SSet.AffineMap.sdIter
+#check SSet.AffineMap.mesh_sd_le
+#check SSet.toTopSdIso
+#check SemiSimplexCategory.sdIso
 
+
+#check CategoryTheory.MorphismProperty.IsCardinalForSmallObjectArgument
 #check TopCat.fibration_of_isOpenCover
 #check SSet.instFibrationTopCatMapToTop_topCatModelCategory
-
 #check SSet.KanComplex.weakEquivalence_iff_of_fibration
 #check TopCat.modelCategory.weakEquivalence_iff_of_fibration
 #check SSet.modelCategoryQuillen.weakEquivalence_iff_of_fibration
